44765d97877585954f282eac52d2919243e24407
NurAlfiana <nuralfianaa346@gmail.com>
2025-09-20 21:58:35 +0700

    Day13.java

---
+++ Day13.java
@@ -0,0 +1,11 @@
+package EvaluasiMentor;
+public class part3 {
+    public static void main(String[] args) {
+        System.out.println("BIODATA MAHASIWA\n_______________________");
+        System.out.println("nama\t\t = fiana");
+        System.out.println("kelas\t\t = b");
+        System.out.println("status\t\t =\"aktif\"");
+        
+        
+    }
+}
