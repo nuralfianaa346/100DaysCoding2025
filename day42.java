abfd824585c1674582884a31a3e540c9b9f98502
NurAlfiana <nuralfianaa346@gmail.com>
2025-10-19 21:39:32 +0700

    day42.java

---
+++ day42.java
@@ -0,0 +1,29 @@
+package isc;
+//
+//import java.util.Scanner;
+//
+//public class day41 {
+//
+//    public static void main(String[] args) {
+//        Scanner a = new Scanner(System.in);
+//        double gajipokok;
+//        double potongan = 0;
+//        double gajibersih;
+//        System.out.println("masukkan gaji pokok :");
+//        gajipokok = a.nextDouble();
+//
+//        if (gajipokok <= 3000000) {
+//            potongan = 0.05;
+//        } else if (gajipokok <= 5000000) {
+//            potongan = 0.10;
+//        } else {
+//            potongan = 0.15;
+//        }
+//
+//        gajibersih = gajipokok - (gajipokok * potongan);
+//
+//        System.out.println("Potongan: " + (potongan * 100) + "%");
+//        System.out.println("Gaji Bersih: Rp" + gajibersih);
+//
+//    }
+//}
