21d91a4bc01550fac270376886823152c9dbd037
NurAlfiana <nuralfianaa346@gmail.com>
2025-12-02 17:05:57 +0700

    day86.java

---
+++ day86.java
@@ -0,0 +1,27 @@
+package isc;
+
+import java.util.Scanner;
+
+public class day86 {
+
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        System.out.print("massukkan elemen: ");
+        int e = a.nextInt();
+
+        int[] arr = new int[e];
+
+        for (int i = 0; i < e; i++) {
+            arr[i] = a.nextInt();
+
+        }
+        int min = arr[0];
+
+        for (int i = 0; i < e; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("angka minimal:" + min);
+    }
+}
